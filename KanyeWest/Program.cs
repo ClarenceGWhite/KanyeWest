de6f29e4aeef4c22723a8fa5e829e186a4c6db5d
clarence <cgwhite76@gmail.com>
2022-11-21 01:03:34 -0600

    Final Version of Kanye/Ron conversation

---
+++ KanyeWest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeWest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            for (int i = 0; i < 5; i++)
+            {
+                var kanyeURL = "https://api.kanye.rest/";
+                var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+                var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+                var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes/";
+                var ronResponse = client.GetStringAsync(ronURL).Result;
+                var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Replace('"', ' ').Trim();
+
+                Console.WriteLine($"Kanye says: {kanyeQuote}");
+                Console.WriteLine($"Ron responded: {ronQuote}");
+                Console.WriteLine();
+
+
+            }
+
+
+
+           
+        }
+    }
+}
